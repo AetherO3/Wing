3af43a15d82a2f0e916b595cc25e2d7eca394345
AetherO3 <utkristhtopwal@gmail.com>
2026-08-27 14:24:28 +0530

    Added SignUp route.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './components/Landing.tsx'
 import Login from './components/Login.tsx'
+import SignUp from './components/SignUp.tsx'
 import { AuthProvider } from './components/AuthProvider.tsx'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
                 <BrowserRouter>
                     <Routes>
                         <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<SignUp/>} />
                         <Route path="/" element={<Landing />} />
                     </Routes >
                 </BrowserRouter >
