80a481eee780ef413bc5c9a4ae682eeed2c569ea
AetherO3 <utkristhtopwal@gmail.com>
2026-09-25 12:55:03 +0530

    Added get group id.

--- backend/src/main/java/com/debateApp/Main/services/UserService.java
+++ backend/src/main/java/com/debateApp/Main/services/UserService.java
@@ -2,7 +2,6 @@ package com.debateApp.Main.services;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-// import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
